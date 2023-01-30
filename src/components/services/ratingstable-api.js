253b0065a3c09d3bf67195cd9d296c12953511f2
fabio.d.mota <fabio.d.mota@cgi.com>
2023-01-30 16:00:46 +0000

    add log

--- src/components/services/ratingstable-api.js
+++ src/components/services/ratingstable-api.js
@@ -4,13 +4,27 @@ import axios from "axios";
 // Actions
 export function getRatingsByYear(Year, token, customerUser) {
   var yearAPI;
-
+  console.log("getRatingsByYear");
   if (Year === "") {
     yearAPI = new Date().getFullYear();
   } else {
     yearAPI = Year;
   }
-
+  console.log(
+    axios
+      .get(process.env.REACT_APP_DASHBOARD_URL_RATINGSTABLE, {
+        params: {
+          year: yearAPI,
+          name: customerUser.name,
+          email: customerUser.email,
+          companyName: customerUser.companyName,
+        },
+        data: customerUser,
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((res) => console.log("res", res))
+      .catch((err) => console.log("err", err))
+  );
   return axios
     .get(process.env.REACT_APP_DASHBOARD_URL_RATINGSTABLE, {
       params: {
