113fd543f2e3c270dfb202e9a18b1af9738780a1
alexsilva <alexandre.b.silva@cgi.com>
2023-01-31 12:13:17 +0000

    Fixed Font Library Imports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.scss";
 import App from "./App";
 import { Provider } from "react-redux";
 import {
@@ -8,10 +7,21 @@ import {
   SharedCssBaseline,
 } from "cx-portal-shared-components";
 import UserService from "./components/services/UserService";
+import { getHostname } from "./components/services/EnvironmentService";
+
+const hostname = getHostname();
+if (hostname === "country-risk-dashboard.dev.demo.catena-x.net")
+  import ('./index-dev.scss');
+if (hostname === "country-risk-dashboard.int.demo.catena-x.net")
+  import ('./index-int.scss');
+else {
+  import ('./index-dev.scss');
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 UserService.init((user) => {
-    console.log(user);
+  console.log(user);
+  console.log("process", process.env);
   root.render(
     <React.StrictMode>
       <SharedCssBaseline />
