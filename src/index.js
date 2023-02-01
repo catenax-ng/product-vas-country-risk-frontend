22397809d2c6722d5e83aa216b24fc420cd50049
fabio.d.mota <fabio.d.mota@cgi.com>
2023-02-01 15:18:33 +0000

    update with tractusx

--- src/index.js
+++ src/index.js
@@ -20,8 +20,7 @@ else {
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 UserService.init((user) => {
-  console.log("process", process.env);
-  console.log(user);
+
   root.render(
     <React.StrictMode>
       <SharedCssBaseline />
